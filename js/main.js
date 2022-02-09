9f8896a1cb003942273cc7069f55b8bf3e29a9b8
Bukovski <kmanankov@bk.ru>
2022-02-09 18:12:30 +0300

    add: header slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+	$(".header-slider").slick({
+		dots: true,
+		prevArrow: "<button type='button' class='slick-prev'><img src='../images/arrow-up.svg' alt='arrow navigation up'></button>",
+		nextArrow: "<button type='button' class='slick-next'><img src='../images/arrow-down.svg' alt='arrow navigation down'></button>",
+	});
+	
+	
 });
